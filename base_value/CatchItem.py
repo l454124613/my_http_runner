0a9f55798641db6ef0fe6634742a65862815ebfd
lixuecheng <lixuechengde@163.com>
2018-06-09 20:08:10 +0800

    add catch

---
+++ base_value/CatchItem.py
@@ -0,0 +1,13 @@
+# -*- coding:utf-8 -*-
+# Author:lixuecheng
+from enum import Enum, unique
+
+
+@unique
+class Catch(Enum):
+    json = 1
+    content = 2
+    status_code=3
+    encoding=4
+    headers=5
+    cookies=6
--- core/base_req.py
+++ core/base_req.py
@@ -3,10 +3,10 @@
 
 from locust.clients import HttpSession, ResponseContextManager
 import json
+from base_value.CatchItem import Catch
 
 
 class MyResponse(ResponseContextManager):
-    encoding = 'UTF-8'
 
     def __init__(self, response, is_locust=True):
         self.__dict__ = response.__dict__
@@ -27,147 +27,58 @@ class MyResponse(ResponseContextManager):
             print('ok', exc)
 
 
-# host = 'http://172.16.32.40:8082'
-# v = HttpSession(host)
-"""Constructs a :class:`Request <Request>`, prepares it and sends it.
-   Returns :class:`Response <Response>` object.
-
-   :param method: method for the new :class:`Request` object.
-   :param url: URL for the new :class:`Request` object.
-   :param params: (optional) Dictionary or bytes to be sent in the query
-       string for the :class:`Request`.
-   :param data: (optional) Dictionary, bytes, or file-like object to send
-       in the body of the :class:`Request`.
-   :param json: (optional) json to send in the body of the
-       :class:`Request`.
-   :param headers: (optional) Dictionary of HTTP Headers to send with the
-       :class:`Request`.
-   :param cookies: (optional) Dict or CookieJar object to send with the
-       :class:`Request`.
-   :param files: (optional) Dictionary of ``'filename': file-like-objects``
-       for multipart encoding upload.
-   :param auth: (optional) Auth tuple or callable to enable
-       Basic/Digest/Custom HTTP Auth.
-   :param timeout: (optional) How long to wait for the server to send
-       data before giving up, as a float, or a :ref:`(connect timeout,
-       read timeout) <timeouts>` tuple.
-   :type timeout: float or tuple
-   :param allow_redirects: (optional) Set to True by default.
-   :type allow_redirects: bool
-   :param proxies: (optional) Dictionary mapping protocol or protocol and
-       hostname to the URL of the proxy.
-   :param stream: (optional) whether to immediately download the response
-       content. Defaults to ``False``.
-   :param verify: (optional) Either a boolean, in which case it controls whether we verify
-       the server's TLS certificate, or a string, in which case it must be a path
-       to a CA bundle to use. Defaults to ``True``.
-   :param cert: (optional) if String, path to ssl client cert file (.pem).
-       If Tuple, ('cert', 'key') pair.
-   :rtype: requests.Response
-"""
-
-
-# def run_http(h, is_locust=True):
-#     header = {"Cookie": 'asdasda=123123'}
-#     with h.request('get', '/webapi/api/token/gettoken?openid=f14f531c-2eef-4550-828b-0bdda49ae9dd',
-#                    catch_response=True, name='getToken') as r:
-#         r1 = MyResponse(r, is_locust)
-#         # print(r1.json())
-#         print(r1.status_code)
-#         # print([k+"="+v for k,v in r1.headers.items()])
-#         # try:
-#         #     print(r1.request.method)
-#         # except Exception as e:
-#         #     print(e)
-#         # try:
-#         #     print(r1.request.url)
-#         # except Exception as e:
-#         #     print(e)
-#         # try:
-#         #     print(r1.request.headers)
-#         # except Exception as e:
-#         #     print(e)
-#         # try:
-#         #     print(r1.request.files)
-#         # except Exception as e:
-#         #     print(e)
-#         # try:
-#         #     print(r1.request.data)
-#         # except Exception as e:
-#         #     print(e)
-#         # try:
-#         #     print(r1.request.json)
-#         # except Exception as e:
-#         #     print(e)
-#         # try:
-#         #     print(r1.request.params)
-#         # except Exception as e:
-#         #     print(e)
-#         # try:
-#         #     print(r1.request.auth)
-#         # except Exception as e:
-#         #     print(e)
-#         # try:
-#         #     print([i for i in r1.request._cookies])
-#         # except Exception as e:
-#         #     print(e)
-#
-#         # print(r1.request)
-#
-#         print(r1.json())
-#
-#         # print(r1.content)
-#         # print([r1.cookies.items()])
-#         # print(r1.ok)
-#         # print(r1.encoding)
-#         # r1.failure('err')
-#         # r1.success('ok')
-#
-#         # print(r1.locust_request_meta)
-#         # print(r1.raise_for_status())
-#         # print(r1.text)
-#
-#         # print(r1.reason)
-#         # print(r1.elapsed)
-#
-#
-# # run_http(v, False)
-
-
 class RunHttp:
-    def __init__(self, host, is_locust=True):
+
+    def __init__(self, host, is_locust=True, http_session=None, catch_dict=None):
         self.host = host
-        self.http_session = HttpSession(host)
+        if http_session is None:
+            self.http_session = HttpSession(host)
+        else:
+            self.http_session = http_session
+
         self.is_locust = is_locust
         self.run_dict = {}
         self.order_dict = {}
         self.res_dict = {}
+        self.catch_dict = {} if catch_dict is None else catch_dict
+        self._add_catch_dict = {}
+        self._add_check_dict = {}
 
     def run(self):
 
         for i in range(len(self.order_dict)):
             name = self.order_dict[str(i + 1)]
             # try:
-            res = self._do_http(self.run_dict[name])
-            self.res_dict[name] = res
-
+            #     self._do_http(self.run_dict[name])
             # except Exception as e:
             #     print('err', e)
+            self._do_http(self.run_dict[name])
 
         class DoRes:
-            def __init__(self, res):
+            def __init__(self, res, catch_res):
                 self._res = res
+                self._catch_res = catch_res
 
-            def get_dict(self):
+            def get_all_result_dict(self):
                 return self._res
 
-            def get_json(self):
+            def get_all_result_json(self):
                 return json.dumps(self._res)
 
-        return DoRes(self.res_dict)
+            def get_catch_dict(self):
+                return self._catch_res
+
+            def get_catch_json(self):
+                return json.dumps(self._catch_res)
+
+        try:
+            return DoRes(self.res_dict, self.catch_dict)
+        except Exception as e:
+            print('结果解析错误', e)
 
     def add_request(self, method, path, name=None, params=None, data=None, headers=None, files=None, auth=None,
-                    timeout=None, allow_redirects=True, proxies=None, json=None, check=None):
+                    timeout=None, allow_redirects=True, proxies=None, json=None, check=None, catch=None,
+                    content_encoding='utf8'):
         info = {}
         if name is None:
             name = path
@@ -190,14 +101,36 @@ class RunHttp:
         info['allow_redirects'] = allow_redirects
         info['proxies'] = proxies
         if self.run_dict.get(name) is None:
-            self.run_dict[name] = {'method': method, 'url': path, 'info': info, 'check': check}
+            self.run_dict[name] = {'method': method, 'url': path, 'info': info, 'check': check, 'catch': catch,
+                                   'content_encoding': content_encoding}
             self.order_dict[str(len(self.order_dict) + 1)] = name
         else:
             print('有重复的名字', name)
         return self
 
-    def _do_http(self, key):
+    def check_res(self, res, check_dict):
 
+        pass
+
+    '''
+    catch_items
+    [{name,catch,value}]
+    '''
+
+    def add_catch(self, catch_items,res):
+        for i in catch_items:
+            if type(i) == dict:
+                if i.get('name') is not None and i.get('catch') is not None and i.get('catch') in Catch and i.get('value') is not None:
+                    pass
+            else:
+                print('在抓取数据时，格式错误：', i)
+
+        pass
+
+    def add_check(self, check_item):
+        pass
+
+    def _do_http(self, key):
         with self.http_session.request(method=key['method'], url=key['url'], catch_response=True, **key['info']) as r:
             r1 = MyResponse(r, self.is_locust)
             if key['check'] is None:
@@ -206,130 +139,90 @@ class RunHttp:
                 else:
                     r1.failure(r1.reason)
             else:
-                if type(key['check']) == dict:
-                    pass
+                if type(key['check']) == list:
+                    if len(key['check']) > 0:
+                        pass
+                    else:
+                        if r1.ok:
+                            r1.success()
+                        else:
+                            r1.failure(r1.reason)
                 else:
-                    r1.failure('校验值输入有误')
-                    raise Exception('校验值输入有误')
+                    r1.failure('校验值输入格式有误')
+                    raise Exception('校验值输入格式有误')
+            if key['catch'] is None:
+                pass
+            else:
+                pass
+            if self.is_locust:
+                pass
+            else:
+                self.res_dict[key['info']['name']] = RunHttp._get_response(r1)
 
-            return RunHttp._get_response(r1)
+            return RunHttp._get_response(r1, key['content_encoding'])
 
     def analysis_res(self):
         pass
 
     @staticmethod
-    def _get_response(res):
-        result = {}
-        # req = {'method': res.request.method, 'url': res.request.url, 'headers': res.request.headers}
-        req = {'method': res.request.method, 'url': res.request.url}
+    def _get_response(res, content_encoding='UTF-8'):
+        req = {}
+
+        req['method'] = res.request.method
+        req['url'] = res.request.url
+
         # print(type(res.request.headers))
         req['headers'] = {k: j for k, j in res.request.headers.lower_items()}
+        result = {}
         result['status_code'] = res.status_code
         try:
             result['json'] = res.json()
         except:
-            result['json'] = 'no'
-        try:
-            result['text'] = res.text
-        except:
-            result['text'] = 'no'
+            result['json'] = False
+        # try:
+        #     result['text'] = res.text
+        # except:
+        #     result['text'] = 'no'
         try:
             req['auth'] = res.request.auth
         except:
-            req['auth'] = 'no'
+            req['auth'] = False
         try:
             req['files'] = res.request.files
         except:
-            req['files'] = 'no'
+            req['files'] = False
         try:
             req['data'] = res.request.data
         except:
-            req['data'] = 'no'
-        try:
-            req['json'] = res.request.json
-        except:
-            req['json'] = 'no'
+            req['data'] = False
         # try:
         #     req['params'] = res.request.params
         # except:
         #     req['params'] = 'no'
-
+        # try:
+        #     req['params'] = res.request.params
+        # except:
+        #     req['params'] = 'no'
+        result['encoding'] = res.encoding
         result['status_code'] = res.status_code
         result['headers'] = {k: j for k, j in res.headers.items()}
-        result['content'] = res.content.decode('utf-8')
-        result['cookies'] = res.cookies.items()
+        result['content'] = res.content.decode(content_encoding)
+        result['cookies'] = {k: j for k, j in res.cookies.items()}
         result['ok'] = res.ok
-        result['encoding'] = res.encoding
 
         result['request_meta'] = res.locust_request_meta
         result['reason'] = res.reason
         result['request'] = req
         result['elapsed'] = res.elapsed.seconds
-        # print(result)
+
         return result
 
-        # print(r1.json())
-        # print(r1.status_code)
-        # print([k+"="+v for k,v in r1.headers.items()])
-        # try:
-        #     print(r1.request.method)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print(r1.request.url)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print(r1.request.headers)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print(r1.request.files)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print(r1.request.data)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print(r1.request.json)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print(r1.request.params)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print(r1.request.auth)
-        # except Exception as e:
-        #     print(e)
-        # try:
-        #     print([i for i in r1.request._cookies])
-        # except Exception as e:
-        #     print(e)
 
-        # print(r1.request)
-
-        # print(r1.json())
-
-        # print(r1.content)
-        # print([r1.cookies.items()])
-        # print(r1.ok)
-        # print(r1.encoding)
-        # r1.failure('err')
-        # r1.success('ok')
-
-        # print(r1.locust_request_meta)
-        # print(r1.raise_for_status())
-        # print(r1.text)
-
-        # print(r1.reason)
-        # print(r1.elapsed)
-
-
-print(RunHttp('http://172.16.32.40:8082', False).add_request('get',
-                                                             '/webapi/api/token/gettoken?openid=f14f531c-2eef-4550-828b-0bdda49ae9dd',
-                                                             name='test').run())
+# print(RunHttp('http://172.16.32.40:8082', False).add_request('get',
+#                                                              '/webapi/api/token/gettoken?openid=f14f531c-2eef-4550-828b-0bdda49ae9dd',
+#                                                              name='test').run())
 # h.add_request('get', '/webapi/api/token/gettoken?openid=f14f531c-2eef-4550-828b-0bdda49ae9dd', name='test')
 # # print(**h.run_list[0])
 # h.do_http(h.run_dict[h.order_dict['1']])
+h = RunHttp('http://www.baidu.com', False).add_request('get', '/', params={'a': 'b'}).run()
+print(h.get_all_result_json())
